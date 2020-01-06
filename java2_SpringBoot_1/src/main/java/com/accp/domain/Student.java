73dea011a6600e9fea29f3919a9f4cb5d891fb0b
liuyi <15364237386@sina.cn>
2020-01-06 11:36:47 +0800

    first

---
+++ java2_SpringBoot_1/src/main/java/com/accp/domain/Student.java
@@ -0,0 +1,43 @@
+package com.accp.domain;
+
+public class Student {
+    private String sid;
+
+    private String sname;
+
+    private String classname;
+
+    private String mid;
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+}
